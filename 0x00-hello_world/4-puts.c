e1fbe0b00f8292bf12e1a0d38b83aab0597965d2
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-15 02:14:28 -0700

    my_first_puts_function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This project is about c
+ * This task is to print a text using 'puts' function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
