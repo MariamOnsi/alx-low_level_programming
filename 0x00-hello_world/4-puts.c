68fa074180a63521feeddf9f0b7b0bfeccb5a1dc
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-15 03:01:04 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -  This project is about c
- * This task is to print a text using 'puts' function
- * Returns: 0 (the task accomplished)
+ * main - this task is to print a text uing puts function.
+ * Description: 'Programming is like building a multilingual puzzle'
+ * Return 0. (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
 }
