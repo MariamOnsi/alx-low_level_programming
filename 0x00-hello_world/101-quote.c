df5587d93e9ce3176e91b4fd775fd54e754e515f
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-15 09:41:32 -0700

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this task is about priting a text withou using printf or puts functions.
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
