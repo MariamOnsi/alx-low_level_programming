53ae796269f27e6e8372592a37ea94b84377edd0
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-16 09:17:06 -0700

    single_digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - taks number 5
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+		printf("%d\n", y);
+	return (0);
+}
