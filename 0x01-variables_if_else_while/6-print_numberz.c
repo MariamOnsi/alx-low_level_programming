f1154f3f76175a2307bb6949b327319dfa79fc8c
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-16 09:34:48 -0700

    #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main task number 6
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
