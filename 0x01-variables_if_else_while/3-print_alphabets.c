740d4618674888cc31bb61be997ee0053e7b5158
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-16 08:25:14 -0700

    lower_upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - this task is about lower ans upper cases.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ls;
+
+	char US;
+
+	ls = 'a';
+	US = 'A';
+	while (ls >= 'z')
+{
+	putchar(ls);
+	ls++;
+}
+while (US >= 'Z')
+{
+	putchar(US);
+	US++;
+}
+putchar('\n');
+return (0);
+}
