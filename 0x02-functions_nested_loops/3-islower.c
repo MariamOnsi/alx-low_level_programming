85df1db5bc82764d4f0e8450d631d04f9c26fb96
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-20 13:18:18 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * _islower(int c) - is a function to check the case of the letters.
+ * _islower - is a function to check the case of the letters.
  * @c: is the case of the letter.
  * Return: 1 if the alpnabets are lowercase retunr 0 for otherwise.
  */
 int _islower(int c)
 {
-int c;
-
 if ((c == 65 && c <= 90) || (c == 97 && c <= 122))
 {
 	return (1);
