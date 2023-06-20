8ddb0eb10f0c8cf8e69e408eee00987d0c777d2c
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-20 07:09:57 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - is a function to print out the absolute value of n.
+ * @my: is the number
+ * Return: n for absolute value, and return x for -ve value.
+ */
+int _abs(int my)
+{
+	if (my < 0)
+{
+	int abs_val1;
+
+	abs_val1 = my * -1;
+	return (abs_val1);
+}
+{
+return (my);
+}
+}
