545c4c4df01ee6556d72283678cc6f9c443c287f
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-20 12:33:10 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - is a function to add two interges.
+ * @number1: onr of the two variables
+ * @number2: the other vaiable.
+ * Return: number1 and number2.
+ */
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}
