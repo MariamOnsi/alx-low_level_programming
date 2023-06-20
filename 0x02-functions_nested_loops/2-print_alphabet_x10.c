68a7eb3e77bbdb176061d6b5e9dc0d94ca2be3c2
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-20 13:46:29 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - task number 2 to pritn the letters 10 times.
- * print_alphabet_x10 - to print the alphabetical order 10 times.
- * @ alphabetical order: to be printed 10 times.
+ * print_alphabet_x10 - task number 2 to pritn the letters 10 times.
  * Return: Always void (Success)
  */
 void print_alphabet_x10(void)
