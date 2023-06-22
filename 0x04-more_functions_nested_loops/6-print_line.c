101ddaf3034e08b6333b8fcaa4d002b295d7514b
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-22 12:22:25 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_line - is a function to print a stright line.
+ * print_line - is a function to print a straight kine
  * @n: is the variable
- * Return: void.
+ * Return: void
  */
 
 void print_line(int n)
 {
+	int c;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int times;
-
-		for (times = 1; times <= n; times++)
+		for (c = 0; c < n; c++)
 		{
-			_putchar('_');
+			_putchar(95);
 		}
+		_putchar('\n');
 	}
 }
