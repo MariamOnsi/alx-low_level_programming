3f1e20f12acc67a1e3aa896ed6b4b33589ce8eec
MariamOnsi <maryam.onsi2012@gmail.com>
2023-06-26 03:33:00 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,15 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0'i)
 	{
+		i++;
+	}
 		for (i -= 1; i >= 0; i--)
 		{
 			putchar(s[i]);
 		}
 		putchar('\n');
-	}
 }
